31dc2d42b652dffcfcce33c659ca0deca577aa0b
Jason Davies <jasondavies@AML0432.local>
2022-05-20 12:19:36 +0100

    first commit

---
+++ src/main/java/com/amido/anagramevaluator/AnagramEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.amido.anagramevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnagramEvaluatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amido/anagramevaluator/AnagramEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amido.anagramevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramEvaluatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
